5638eba12f10606ed51a77ac8829ed4ab248dcfe
satinder <satindersingh472@gmail.com>
2022-07-26 20:06:45 -0600

    debugger is showing tags around title and body

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function success_function(response){
+    let count = 0;
+for (count = 0; count < response[`data`].length;count++){
+    response[`data`][count][`title`] = `<h3> ${response[`data`][count][`title`]} </h3>`;
+    response[`data`][count][`body`] = `<p>${response[`data`][count][`body`]}</p>`;
+}
+}
+function failure_function(error){
+info[`innerHTML`] = `<p>An error has occured</p>`;
+}
+
+axios.request({
+    url: `https://jsonplaceholder.typicode.com/posts`
+}).then(success_function).catch(failure_function);
+
+let info = document.getElementById(`info`);
