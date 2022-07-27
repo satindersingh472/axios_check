8ed20827330ebb396738644383a2bb631ad9f86c
satinder <satindersingh472@gmail.com>
2022-07-26 20:17:25 -0600

    updated

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-function success_function(response){
+function success_function(response) {
     let count = 0;
-for (count = 0; count < response[`data`].length;count++){
-    response[`data`][count][`title`] = `<h3> ${response[`data`][count][`title`]} </h3>`;
-    response[`data`][count][`body`] = `<p>${response[`data`][count][`body`]}</p>`;
+    for (count = 0; count < response[`data`].length; count++) {
+        response[`data`][count][`title`] = `<h3> ${response[`data`][count][`title`]} </h3>`;
+        response[`data`][count][`body`] = `<p>${response[`data`][count][`body`]}</p>`;
+        console.log(response[`data`][count][`title`]);
+        console.log(response[`data`][count][`body`]);
+    }
 }
-}
-function failure_function(error){
-info[`innerHTML`] = `<p>An error has occured</p>`;
+function failure_function(error) {
+    info[`innerHTML`] = `<p>An error has occured</p>`;
 }
 
 axios.request({
